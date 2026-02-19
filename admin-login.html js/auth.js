f7e83a221659768e5e838d805541315815d48d5b
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-20 02:09:30 +0800

    Create auth.js

---
+++ admin-login.html js/auth.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Admin Login</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="card center">
+  <h1>🔐 Admin Login</h1>
+  <input type="password" id="adminPass" placeholder="Masukkan Password">
+  <button onclick="loginAdmin()">Login</button>
+  <div id="loginMsg"></div>
+</div>
+
+<script src="js/config.js"></script>
+<script src="js/api.js"></script>
+<script src="js/auth.js"></script>
+
+</body>
+</html>
