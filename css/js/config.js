bfc902987594fcf47223f0da4b717878e9165912
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-20 01:57:09 +0800

    Create config.js

---
+++ css/js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  API_BASE: "https://script.google.com/macros/s/AKfycbwgNWH0l8CDCg-Eq9eaZ2saLzIYVlP300kP3BOnDYmv4NMwkH32M9rRyL9Yh1k154M6/exec",
+  API_TOKEN: "GQS2026HQSECURE2026"
+};
