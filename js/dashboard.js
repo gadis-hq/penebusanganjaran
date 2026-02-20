2d543ae335e0fdc84493bc47524c676fbacd01c0
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-21 02:02:00 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,20 @@
+if(!localStorage.getItem("adminToken")){
+  window.location.href="admin-login.html";
+}
 
+async function loadDashboard(){
+
+  try{
+    const data = await apiRequest({
+      action:"dashboardStats",
+      token: localStorage.getItem("adminToken")
+    });
+
+    console.log(data);
+
+  }catch(err){
+    console.error("Dashboard error",err);
+  }
+}
+
+loadDashboard();
