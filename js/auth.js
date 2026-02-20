0177b0ab43554f99aadb473c7389715e0c1f77ff
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-21 02:02:53 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,20 @@
+async function login(username,password){
 
+  const data = await apiRequest({
+    action:"adminLogin",
+    username:username,
+    password:password
+  });
+
+  if(data.success){
+    localStorage.setItem("adminToken",data.token);
+    return true;
+  }
+
+  return false;
+}
+
+function logout(){
+  localStorage.removeItem("adminToken");
+  window.location.href="admin-login.html";
+}
