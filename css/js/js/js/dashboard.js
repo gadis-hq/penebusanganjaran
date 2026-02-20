3904583475548edc42c2432142540fcdc76c73c2
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-21 00:43:29 +0800

    Delete css directory

--- css/js/js/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-async function loadDashboard(){
-  const data = await apiRequest({action:"stats"});
-  document.getElementById("totalAktif").innerText=data.status.aktif;
-  document.getElementById("totalDitebus").innerText=data.status.ditebus;
-  document.getElementById("totalSemua").innerText=data.total;
-}
-setInterval(loadDashboard,5000);
-loadDashboard();
