6e630aaf92dc99daaa1a6d45f0137d435f6b50ee
Gadis QS HQ <qsgadis449@gmail.com>
2026-02-21 02:00:10 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,16 @@
+async function apiRequest(payload){
 
+  const response = await fetch(CONFIG.API_URL,{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(payload)
+  });
+
+  if(!response.ok){
+    throw new Error("Network error");
+  }
+
+  return await response.json();
+}
